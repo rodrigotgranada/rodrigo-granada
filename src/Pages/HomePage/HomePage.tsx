aade9d8f30bc12b9c124748197719901c77e0f51
Rodrigo Granada <rodrigotgranada@gmail.com>
2024-05-14 15:18:35 -0300

    Display Home

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,16 +1,20 @@
 
-// import Button from "../../Components/Button/Button";
+import "./HomePage.scss"
+import selfie from "./../../Assets/selfie.jpg"
 
 
 const HomePage = () => {
-    // const { darkTheme, toggleTheme } = useContext(
-    //     ThemeContext
-    // ) as ThemeContextInterface;
-
     return (
-        <div>
-            Home
-            {/* <Button children={`Teste`} types={'geral'} onClick={() => console.log('cliquei')} /> */}
+        <div className="flex flex-col-reverse justify-evenly md:flex-row text-center">
+            <div className="p-4  md:text-left">
+                <div className="text-7xl font-semibold">Olá!</div>
+                <div className="text-6xl">Me chamo <span className="text-red-500">Rodrigo</span>.</div>
+                <div className="text-xl">Desenvolvedor Front-end.</div>
+            </div>
+            <div className="rounded ">
+                <img src={String(selfie)} alt="Eu" className="rounded-lg m-auto w-[11rem]" />
+            </div>
+
         </div>
     )
 }
